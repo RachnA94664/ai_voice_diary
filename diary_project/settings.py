c16fb81f94807c99f30cdffcf5b605b37ff474fc
Rachna Verma <rv1249367@gmail.com>
2025-11-19 10:39:59 +0530

    Created database models and registered in admin

--- diary_project/settings.py
+++ diary_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'diary',
+    'expenses',
+    'accounts',
 ]
 
 MIDDLEWARE = [
